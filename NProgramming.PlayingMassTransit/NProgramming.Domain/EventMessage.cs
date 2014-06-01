6951995e875347b8750b1407c50c7e59304b7975
Jacek Spolnik <jacek.spolnik@gmail.com>
2014-06-01 17:24:01 +0200

    add new project
    domain

---
+++ NProgramming.PlayingMassTransit/NProgramming.Domain/EventMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NProgramming.Domain
+{
+    [Serializable]
+    public class EventMessage
+    {
+        public EventMessage()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Id: {0}, Value: {1}", Id, Value);
+        }
+    }
+}
